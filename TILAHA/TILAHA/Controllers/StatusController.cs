cf0b52acf99862065934cb5292668d71a1f955cf
juha62 <juha.m.pusa@hotmail.com>
2017-05-08 18:14:27 +0300

    Muutama controlleri ja näkymä luotu. Haku tietokannasta toimii ja puppu data kannasta tulostuu ruudulle aika nätisti.

---
+++ TILAHA/TILAHA/Controllers/StatusController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TILAHA.Controllers.Models;
+
+namespace TILAHA.Controllers
+{
+    public class StatusController : Controller
+    {
+        // GET: Status
+        public ActionResult Index()
+        {
+            LaitekantaEntities entities = new LaitekantaEntities();
+            List<Status> model = entities.Status.ToList();
+            entities.Dispose();
+            return View(model);
+          
+        }
+    }
+}
---
+++ TILAHA/TILAHA/Controllers/VarusteetController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TILAHA.Controllers.Models;
+
+namespace TILAHA.Controllers
+{
+    public class VarusteetController : Controller
+    {
+        // GET: Varusteet
+        public ActionResult Index()
+        {
+            LaitekantaEntities entities = new LaitekantaEntities();
+            List<Varuste> model = entities.Varuste.ToList();
+            entities.Dispose();
+            return View(model);
+           
+        }
+    }
+}
