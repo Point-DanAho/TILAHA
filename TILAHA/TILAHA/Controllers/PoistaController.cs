1e6830732e5765d29f5a72500cc66894a8e6f30a
juha62 <juha.m.pusa@hotmail.com>
2017-05-18 20:26:22 +0300

    Lisätty Poista-kontrolleri ja muutettu Web congiin oman koneen SQL serverin osoite. Kannattaa muistaa muuttaa web congiin oma SQL serverin osoite, niin taulut löytyy sitten ok.

---
+++ TILAHA/TILAHA/Controllers/PoistaController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TILAHA.Controllers.Models;
+
+namespace TILAHA.Controllers
+{
+    public class PoistaController : Controller
+    {
+        // GET: Poista
+        public ActionResult Index()
+        {
+            LaitekantaEntities entities = new LaitekantaEntities();
+            List<Laite> model = entities.Laite.ToList();
+            entities.Dispose();
+            return View(model);
+        }
+    }
+}
