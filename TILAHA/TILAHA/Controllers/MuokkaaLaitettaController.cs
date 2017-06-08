b6ce4aba187ad9ab0bf107a17e0318b73c95182a
UlrikaKorpinen <Ulrika.korpinen@stud.pointcollege.fi>
2017-06-08 17:00:53 +0300

    Luotu uusi MuokkaaLaitettaController kun vanha hävinnyt

---
+++ TILAHA/TILAHA/Controllers/MuokkaaLaitettaController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using TILAHA.Controllers.Models;
+
+namespace TILAHA.Controllers
+{
+    public class MuokkaaLaitettaController : Controller
+    {
+        private WarehouseDBEntities db = new WarehouseDBEntities();
+
+        // GET: MuokkaaLaitetta
+        public ActionResult Index()
+        {
+            return View(db.Laitteet.ToList());
+        }
+
+        // GET: MuokkaaLaitetta/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Laitteet laitteet = db.Laitteet.Find(id);
+            if (laitteet == null)
+            {
+                return HttpNotFound();
+            }
+            return View(laitteet);
+        }
+
+        // GET: MuokkaaLaitetta/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: MuokkaaLaitetta/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "DeviceId,WarrantyId,Make,Model,SN,Condition,DeviceType,AddInfo,PurcDate,WarrEndDate,PurchPrize")] Laitteet laitteet)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Laitteet.Add(laitteet);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(laitteet);
+        }
+
+        // GET: MuokkaaLaitetta/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Laitteet laitteet = db.Laitteet.Find(id);
+            if (laitteet == null)
+            {
+                return HttpNotFound();
+            }
+            return View(laitteet);
+        }
+
+        // POST: MuokkaaLaitetta/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "DeviceId,WarrantyId,Make,Model,SN,Condition,DeviceType,AddInfo,PurcDate,WarrEndDate,PurchPrize")] Laitteet laitteet)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(laitteet).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(laitteet);
+        }
+
+        // GET: MuokkaaLaitetta/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Laitteet laitteet = db.Laitteet.Find(id);
+            if (laitteet == null)
+            {
+                return HttpNotFound();
+            }
+            return View(laitteet);
+        }
+
+        // POST: MuokkaaLaitetta/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Laitteet laitteet = db.Laitteet.Find(id);
+            db.Laitteet.Remove(laitteet);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
